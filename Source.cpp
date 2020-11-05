6bb45ee651c084113af64b6789592fbdaa0e4013
LytvynenkoJ <70521634+LytvynenkoJ@users.noreply.github.com>
2020-11-05 18:01:59 +0300

    Основной файл
    Опять таки пока для k=4

---
+++ Source.cpp
@@ -0,0 +1,62 @@
+#include "Header.h"
+
+int main()
+{
+	setlocale(LC_ALL,"Russian");
+	cout << t << endl;
+	string firstNum;
+	string secondNum;
+	int* first = new int[t];
+	int* second = new int[t];
+	int* sum = new int[t];
+	int* mul = new int[t];
+	int* inv = new int[t];
+	first = obnul(first,t);
+	second = obnul(second, t);
+	sum = obnul(sum, t);
+	mul = obnul(mul, t);
+	inv = obnul(inv, t);
+	mod = obnul(mod, t);
+	cout << "Введите первый элемент:   ";
+	cin >> firstNum;
+	first = strToArr(firstNum);
+	cout << "Введите второй элемент:   ";
+	cin >> secondNum;
+	second = strToArr(secondNum);
+
+	//k=4
+	mod[2] = 1;
+	mod[5] = 1;
+	mod[6] = 1;
+	cout << "Модуль:   ";
+	outArr(mod, k+1);
+	cout << endl;
+
+	// sum
+	sum = Add(first,second);
+	cout << "Сумма:   ";
+	outArr(sum,k);
+	cout << endl;
+
+	// multiple
+	mul = mult(first,second);
+	cout << "Произведение:   ";
+	outArr(mul, k);
+	cout << endl;
+
+	cout << "q:   ";
+	outArr(q, k);
+	cout << endl;
+
+	inv = inverse(first);
+	cout << "Обратный:   ";
+	outArr(inv, k);
+	cout << endl;
+
+	delete[] first;
+	delete[] second;
+	delete[] sum;
+	delete[] mod;
+	delete[] mul;
+	return 0;
+}
