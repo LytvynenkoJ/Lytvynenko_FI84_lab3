5b99199be4e5a6dc9fd1886dae05a53c26b52acc
LytvynenkoJ <70521634+LytvynenkoJ@users.noreply.github.com>
2020-11-08 21:10:17 +0300

    Основной файл
    Конечный результат, проверенный для k=239

--- Source.cpp
+++ Source.cpp
@@ -31,9 +31,11 @@ int main()
 	second = strToArr(secondNum);
 
 	//k=4
-	mod[2] = 1;
-	mod[5] = 1;
-	mod[6] = 1;
+	mod[237] = 1;
+	mod[461] = 1;
+	mod[474] = 1;
+	mod[475] = 1;
+	mod[476] = 1;
 	cout << "Модуль:   ";
 	outArr(mod, k+1);
 	cout << endl;
@@ -43,26 +45,40 @@ int main()
 	cout << "Сумма:   ";
 	outArr(sum,k);
 	cout << endl;
+	cout << dec << "Время работы суммы в наносекундах : "
+		<< chrono::duration_cast<chrono::nanoseconds>(endtime - start).count()
+		<< " ns" << endl << endl;
 
 	// multiple
 	mul = mult(first,second);
 	cout << "Произведение:   ";
 	outArr(mul, k);
 	cout << endl;
-
-	cout << "q:   ";
-	outArr(q, k);
-	cout << endl;
+	cout << dec << "Время работы произведения в наносекундах : "
+		<< chrono::duration_cast<chrono::nanoseconds>(endtime - start).count()
+		<< " ns" << endl << endl;
 
 	square = Squa(first);
 	cout << "Квадрат:   ";
 	outArr(square, k);
 	cout << endl;
+	cout << dec << "Время работы квадрата в наносекундах : "
+		<< chrono::duration_cast<chrono::nanoseconds>(endtime - start).count()
+		<< " ns" << endl << endl;
 
 	tr = trace(first);
 	cout << "След:   ";
-	outArr(tr, k);
-	cout << endl;
+	if (compare(tr,NulEl())==0)
+	{
+		cout << "0" << endl;
+	}
+	if (compare(tr, OneEl()) == 0)
+	{
+		cout << "1" << endl;
+	}
+	cout << dec << "Время работы функции следа в наносекундах : "
+		<< chrono::duration_cast<chrono::nanoseconds>(endtime - start).count()
+		<< " ns" << endl << endl;
 
 	cout << "В какую степень вы хотите вознести первый полином? (в двоичном виде, не более чем 2^{n}-1)" << endl;
 	cin >> deg;
@@ -71,11 +87,17 @@ int main()
 	cout << "Степень:   ";
 	outArr(degree, k);
 	cout << endl;
-	
+	cout << dec << "Время работы степени в наносекундах : "
+		<< chrono::duration_cast<chrono::nanoseconds>(endtime - start).count()
+		<< " ns" << endl << endl;
+
 	inv = inverse(first);
 	cout << "Обратный:   ";
 	outArr(inv, k);
 	cout << endl;
+	cout << dec << "Время работы нахождения обратного в наносекундах : "
+		<< chrono::duration_cast<chrono::nanoseconds>(endtime - start).count()
+		<< " ns" << endl << endl;
 
 	delete[] first;
 	delete[] second;
